ac0a61cf8d22660d391de1982d8c1ce274aeff8f
Redaccx <105161552+Redaccx@users.noreply.github.com>
2025-08-01 23:15:59 +0300

    Create data.js

---
+++ Id-system/data.js
@@ -0,0 +1,18 @@
+// data.js
+const idCards = [
+  {
+    id: "LMDA-001",
+    name: "Ali Ahmad",
+    position: "متطوع"
+  },
+  {
+    id: "LMDA-002",
+    name: "Rana Hassan",
+    position: "منسقة مشاريع"
+  },
+  {
+    id: "LMDA-003",
+    name: "Mohammad Kassem",
+    position: "مشرف ميداني"
+  }
+];
